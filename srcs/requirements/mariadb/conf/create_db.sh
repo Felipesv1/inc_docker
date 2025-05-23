375cd672d45338883e2de7e380c808b26f1e2d10
felperei <fepereirasilva4@gmail.com>
2025-05-23 16:01:51 -0300

    modified

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -5,10 +5,6 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
         # init database
         mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
 
-        tfile=`mktemp`
-        if [ ! -f "$tfile" ]; then
-                return 1
-        fi
 fi
 
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
@@ -17,14 +13,14 @@ if [ ! -d "/var/lib/mysql/wordpress" ]; then
 USE mysql;
 FLUSH PRIVILEGES;
 DELETE FROM     mysql.user WHERE User='';
-DELETE FROM     mysql.user WHERE User='wordpress_user';
+DELETE FROM     mysql.user WHERE User='${DB_USER}';
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
 CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
-GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
         /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
